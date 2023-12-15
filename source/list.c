a712bcfb2c6f668ce6ff3c998f59ff5d68a9e9d6
Cixo Develop <cixolaptop@gmail.com>
2023-12-15 01:29:00 +0100

    Work on list, add basic functions

--- source/list.c
+++ source/list.c
@@ -1,1 +1,61 @@
 #include "list.h"
+
+size_t cl_list_count(cl_list_t self) {
+    size_t list_size = 0;
+    cl_list_item_t *current = self; 
+
+    while (cl_list_item_exists(current)) {
+        current = current->next;
+        ++list_size;
+    }
+
+    return list_size;
+}
+
+cl_list_item_t* cl_list_get_last(cl_list_t self) {
+    cl_list_item_t *current = self;
+
+    if (current == NULL) return current;
+
+    while (current->next != NULL) {
+        current = current->next;
+    }   
+
+    return current;
+}
+
+static inline cl_list_item_t* cl_list_item_create() {
+    return (cl_list_item_t *) malloc(sizeof(cl_list_item_t));
+}
+
+bool cl_list_append(cl_list_t *self, cl_object_t new_object) {
+    cl_list_item_t *new_item = cl_list_item_create();
+
+    if (new_item == NULL) return false;
+
+    new_item->content = new_object;
+    new_item->next = NULL;
+
+    cl_list_item_t *last = cl_list_get_last(*self);
+
+    if (last == NULL) {
+        self = new_item;
+        return true;
+    }
+
+    last->next = new_item;
+    return true;
+}
+
+cl_list_item_t* cl_list_get(cl_list_t self, size_t count) {
+    cl_list_item_t *current = self;
+
+    while (count > 0) {
+        if (current->next == NULL) return NULL;
+        
+        current = current->next;
+        --count;
+    }
+
+    return current;
+}
--- source/list.h
+++ source/list.h
@@ -1,6 +1,8 @@
 #ifndef CL_LIST_H_INCLUDED
 #define CL_LIST_H_INCLUDED
 
+#include <stdint.h>
+#include <stdbool.h>
 #include "object.h"
 
 typedef struct cl_list_item_s {
@@ -8,6 +10,19 @@ typedef struct cl_list_item_s {
     struct cl_list_item_s *next;
 } cl_list_item_t;
 
+typedef cl_list_item_t* cl_list_t;
 
+static inline cl_list_t cl_list_create() {
+    return NULL;
+}
+
+static inline bool cl_list_item_exists(cl_list_item_t *self) {
+    return self != NULL;
+}   
+
+size_t cl_list_count(cl_list_t self);
+cl_list_item_t* cl_list_get_last(cl_list_t self);
+cl_list_item_t* cl_list_get(cl_list_t self, size_t count);
+bool cl_list_append(cl_list_t *self, cl_object_t new_object);
 
 #endif
